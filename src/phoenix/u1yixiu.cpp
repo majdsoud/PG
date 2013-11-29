21c24b9d626b3559cfee919c663d5f9b193b15b0
lucktroy <jbzhang86@gmail.com>
2013-11-29 08:36:38 +0800

    small update

--- src/phoenix/u1yixiu.cpp
+++ src/phoenix/u1yixiu.cpp
@@ -9,20 +9,16 @@
 #define TIMING
 
 int lineAll = 0;
-
-std::ofstream foutx("X.txt");
-std::ofstream fouty("Y.txt");
+std::ofstream fout("X.txt");
 
 struct stringMeta {
-	char* str1;
-	char* str2;
-	char* str3;
+	char* str;
 	int id;
 
-	stringMeta() { str1 = NULL; str2 = NULL; str3 = NULL; id  = -1; }
-    stringMeta(char* s1,char* s2,char* s3, int id) { this->id = id; this->str1 = s1;this->str2 = s2;this->str3 = s3; }
+	stringMeta() { str = NULL; id  = -1; }
+    stringMeta(char* s, int id) { this->id = id; this->str = s; }
 
-    //void dump() { printf("#%d:%s\n",id,str1); }
+    void dump() { printf("#%d:%s\n",id,str); }
 };
 
 
@@ -35,7 +31,7 @@ public:
 
     void *locate (data_type *data, uint64_t len) const
     {
-        return data->str1;
+        return data->str;
     }
 
     void map(data_type& p, map_container& out) const
@@ -43,25 +39,16 @@ public:
 		std::string word1= "";
 		std::string word2;
 		int n =0;
-		//printf("1:%s 2:%s 3:%s\n",p.str1,p.str2,p.str3);
 		for(int i = 0; i < lineAll ; i++) 
-			if(p.id!=i){
-				if (strcmp(p.str1,strMeta[i].str1) == 0)
-				{
-					if (strcmp(p.str2,strMeta[i].str2) == 0)
-					{
-						if (strcmp(p.str3,strMeta[i].str3) == 0)
-						{
-							word1 += "\t";
-							std::stringstream stmp1; 
-							stmp1 << (i+1);
-							std::string tmp1 = stmp1.str();
-							n++;
-							word1 += tmp1;
-						}
-					}
+			if(p.id!=i)
+				if (strcmp(p.str,strMeta[i].str) == 0){
+					word1 += "\t";
+					std::stringstream stmp1; 
+					stmp1 << (i+1);
+					std::string tmp1 = stmp1.str();
+					n++;
+					word1 += tmp1;
 				}
-			}
 		std::stringstream stmp2; 
 		stmp2 << (p.id+1);
 		std::string tmp2=stmp2.str();
@@ -79,7 +66,7 @@ public:
 		word2 += "\n";
 		//std::cout<<(p.id+1)<<" "<<word2<<std::endl;
         emit_intermediate(out, (p.id+1), word2);
-		//foutx << word2;
+		//fout << word2;
     }
 };
 
@@ -96,56 +83,41 @@ int main(int argc, char *argv[])
 	
 	std::ifstream fin(fname);
 	char line[100]={};
-	std::string x = "";
-	std::string y = "";
-	std::string z = "";
-	std::string yy = "";
+	//std::string x = "";
+	//std::string y = "";
+	//std::string z = "";
 	int lineNum = 0;
-	char temp1[lineAll][10];
-	char temp2[lineAll][10];
-	char temp3[lineAll][10];
-	int *yyy = (int *)malloc(lineAll*sizeof(int));
-
+	char tmp[lineAll][10];
 	while(fin.getline(line, sizeof(line)))
 	{
-		std::stringstream word(line);
-		word >> x;
-		word >> y;
-		word >> z;
-		word >> yy;
-		if(strcmp(yy.c_str(),"y")==0)
-			yyy[lineNum] = 1;
-		else 
-			yyy[lineNum] = 0;
-		strcpy( temp1[lineNum], x.c_str());
-		strcpy( temp2[lineNum], y.c_str());
-		strcpy( temp3[lineNum], z.c_str());
-		strMeta[lineNum] = stringMeta(temp1[lineNum],temp2[lineNum],temp3[lineNum],lineNum);
+		//	std::stringstream word(line);
+		//	word >> x;
+		//	word >> y;
+		//	word >> z;
+		//tmp = x.c_str();
+		//tmp[lineNum] = tmpp.c_str();typedef std::string wc_word;
+		std::string tmpp;
+		tmpp= line;
+		strcpy( tmp[lineNum], tmpp.c_str());
+		strMeta[lineNum] = stringMeta(tmp[lineNum],lineNum);
 		lineNum++;
+
 	}
 	get_time (end);
     print_time("reading:", begin, end);
 
-fouty<<"2\t"<<lineAll<<std::endl;
-fouty<<"1\t0"<<std::endl;
-for (int i=0;i<lineAll;i++){
-	if(yyy[i])
-		fouty<<"1\t0\n";
-	else
-		fouty<<"0\t1\n";
-}
-
 	get_time (begin);
 	YixiuMR* mapReduce = new YixiuMR(strMeta);
-	std::vector<YixiuMR::keyval> result;
+	std::vector<YixiuMR::keyval> result;      
 	CHECK_ERROR(mapReduce->run(strMeta,lineAll,result) < 0);
 	get_time (end);
     print_time("calculating:", begin, end);
 
+
     for (int  i = 0; i < lineAll; i++)
     {
-		//foutx << result[i].val << "\t";
-		foutx << result[i].val;
+		//fout << result[i].val << "\t";
+		fout << result[i].val;  
     }
 
 	fin.clear();
