cbb7a79ceabca40908cc6e8ef820238e5d221f5b
lucktroy <jbzhang86@gmail.com>
2013-12-04 06:59:36 -0600

    all

--- runTest4.sh
+++ runTest4.sh
@@ -1,6 +1,6 @@
 export GRAPHCHI_ROOT=../engine/graphchi-cpp
 BIN=../bin
 cd test4
-$BIN/src/phoenix/yixiu input.txt 10000
+$BIN/src/phoenix/yixiu input.txt 100000
 $BIN/src/graphchi/matrix_multiply file1 X.txt file2 Y.txt
 cd ..
