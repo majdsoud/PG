f3e3f9c82a0f2ce1de36881ea843b9f43af45c8e
lucktroy <jbzhang86@gmail.com>
2013-12-15 23:34:45 +0800

    small update

--- src/DefinesPG.mk
+++ src/DefinesPG.mk
@@ -9,3 +9,10 @@ CPPFLAGS = -g -O3 $(INCFLAGS)  -fopenmp -Wall -Wno-strict-aliasing
 LINKERFLAGS = -lz
 DEBUGFLAGS = -g -ggdb $(INCFLAGS)
 HEADERS=$(shell find $(GC_HOME) -name '*.hpp')
+
+INCLUDE_GC=$(GC_HOME)/example_apps #gc: GraphChi
+
+include $(PX_HOME)/Defines.mk
+
+LIBS += -L$(PX_HOME)/$(LIB_DIR) -l$(PHOENIX)
+
