cffb85e561436543a25302638278499453f85a80
lucktroy <jbzhang86@gmail.com>
2013-12-15 10:59:01 -0600

    all

--- runTest.sh
+++
@@ -1,6 +0,0 @@
-export GRAPHCHI_ROOT=../engine/graphchi-cpp
-BIN=../bin
-cd test
-$BIN/src/phoenix/yixiu input.txt 10000
-$BIN/src/graphchi/matrix_multiply file1 X.txt file2 y.txt
-cd ..
--- runTest4.sh
+++
@@ -1,6 +0,0 @@
-export GRAPHCHI_ROOT=../engine/graphchi-cpp
-BIN=../bin
-cd test4
-$BIN/src/phoenix/yixiu input.txt 100000
-$BIN/src/graphchi/matrix_multiply file1 X.txt file2 Y.txt
-cd ..
