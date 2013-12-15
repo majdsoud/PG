2b3880758bdcdb6a405b04efea961c881b1ae746
lucktroy <jbzhang86@gmail.com>
2013-12-16 00:56:58 +0800

    all

--- runPhoenixOnly.sh
+++ runPhoenixOnly.sh
@@ -6,6 +6,6 @@ NAME=testPhoenix
 mkdir -p ${NAME}
 cd ${NAME}
 ${BIN}/gm_dense_phoenix -f ${DATA}/bank_10.txt -r 10 -a 11 -n 5 -c 1 -e 2
-${BIN}/mm_dense_phoenix X.txt Y.txt 10 2 1
+${BIN}/mm_dense_phoenix matrix_file_A.txt  matrix_file_B.txt 10 2 1
 cd -
 
