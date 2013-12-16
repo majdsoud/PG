d52dd4edfad9d2e2d158e167b45e311bafecec4d
lucktroy <jbzhang86@gmail.com>
2013-12-16 18:44:30 +0800

    all

--- runHybrid.sh
+++ runHybrid.sh
@@ -5,7 +5,7 @@ DATA=../data
 NAME=testHybird
 mkdir -p ${NAME}
 cd ${NAME}
-${BIN}/gm_sparse_phoenix -f ${DATA}/bank_10.txt -r 10 -a 11 -n 5 -c 1 -e 2
-${BIN}/mm_sparse_graphchi file1 X.txt file2 Y.txt
+${BIN}/gm_sparse_phoenix -f ${DATA}/bank_10.txt -r 10 -a 11 -n 5 -c 1 -e 2 1>ret.out 2>ret.time
+${BIN}/mm_sparse_graphchi file1 X.txt file2 Y.txt 1>>ret.out 2>>ret.time
 cd -
 
--- runPhoenixOnly.sh
+++ runPhoenixOnly.sh
@@ -5,7 +5,7 @@ DATA=../data
 NAME=testPhoenix
 mkdir -p ${NAME}
 cd ${NAME}
-${BIN}/gm_dense_phoenix -f ${DATA}/bank_10.txt -r 10 -a 11 -n 5 -c 1 -e 2
-${BIN}/mm_dense_phoenix matrix_file_A.txt  matrix_file_B.txt 10 2 1
+${BIN}/gm_dense_phoenix -f ${DATA}/bank_10.txt -r 10 -a 11 -n 5 -c 1 -e 2 1>ret.out 2>ret.time
+${BIN}/mm_dense_phoenix matrix_file_A.txt  matrix_file_B.txt 10 2 1 1>>ret.out 2>>ret.time
 cd -
 
